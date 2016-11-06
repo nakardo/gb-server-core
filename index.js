b84d67d259bd3f053bad8100ef68a125f09e7468
Diego Acosta <diegonake@gmail.com>
2016-11-06 23:08:14 +0100

    cleanup

--- index.js
+++ index.js
@@ -8,26 +8,6 @@ const io = require('socket.io')(server);
 const Gameboy = require('node-gameboy');
 
 
-// Server
-
-app.use(express.static('public'))
-server.listen(3000);
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
-
-let peers = 0;
-io.on('connection', (socket) => {
-    if (++peers == 1) gameboy.pauseResume();
-    socket.on('disconnect', () => {
-        if (!--peers) gameboy.pauseResume();
-    });
-
-    socket.on('keydown', (keyCode) => gameboy.joypad.keyDown(keyCode));
-    socket.on('keyup', (keyCode) => gameboy.joypad.keyUp(keyCode));
-});
-
 // Emulator
 
 const gameboy = new Gameboy();
@@ -40,3 +20,25 @@ gameboy.gpu.on('frame', (canvas) => {
     if (++i % 2) return;
     io.emit('frame', canvas.toDataURL());
 });
+
+// Server
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+let peers = 0;
+
+io.on('connection', (socket) => {
+    if (++peers == 1) gameboy.pauseResume();
+    socket.on('disconnect', () => {
+        if (!--peers) gameboy.pauseResume();
+    });
+
+    socket.on('keydown', (keyCode) => gameboy.joypad.keyDown(keyCode));
+    socket.on('keyup', (keyCode) => gameboy.joypad.keyUp(keyCode));
+});
+
+server.listen(3000);
