ea8710ffd0537fcae14132ac2cffc6231e7369ec
Diego Acosta <diegonake@gmail.com>
2016-12-01 22:31:56 +0100

    pause instance and autosave

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ redis.lrange('states', 0, 1, (err, result) => {
     if (err) throw err;
     gameboy.loadCart(fs.readFileSync(process.env.ROM_PATH));
     gameboy.start();
+    gameboy.pauseResume();
 
     if (result.length) {
         debug('loading state');
@@ -33,27 +34,39 @@ gameboy.gpu.on('frame', (canvas) => {
     i = 0;
 });
 
+function saveState (cb = () => {}) {
+    debug('saving state');
+    redis.lpush('states', JSON.stringify(gameboy), cb);
+}
+
+setInterval(saveState, 1000 * 60 * 60 * 24);
+
 // Server
 
 app.use(express.static('public'));
 app.get('/ping', (req, res) => res.sendStatus(200));
 
+let peers = 0;
+
 io.on('connection', (socket) => {
+    if (++peers == 1) gameboy.pauseResume();
+    socket.on('disconnect', () => {
+        if (!--peers) gameboy.pauseResume();
+    });
+
     socket.on('keydown', (keyCode) => gameboy.joypad.keyDown(keyCode));
     socket.on('keyup', (keyCode) => gameboy.joypad.keyUp(keyCode));
 });
 
 process.on('SIGTERM', () => {
-    try {
-        redis.lpush('states', JSON.stringify(gameboy), (err) => {
-            if (err) process.exit(1);
-            debug('exited saving state');
-            process.exit(0);
-        });
-    } catch (e) {
-        debug('exited with error: %s', e);
-        process.exit(1);
-    }
+    saveState((err) => {
+        if (err) {
+            debug('exited with error: %s', err);
+            process.exit(1);
+        }
+        debug('exited saving state');
+        process.exit(0);
+    });
 });
 
 server.listen(process.env.PORT || 3000);
